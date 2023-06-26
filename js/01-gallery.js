2a3963e49fddc6c2fcfb2ef0a19b42d5dd0a7561
Santiago <santigomas20@gmail.com>
2023-06-25 23:32:15 -0500

    end homework

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+const ulList = document.querySelector('.gallery');
+const markup = galleryItems
+  .map(item => {
+    return `<li class="gallery__item">
+    <a class="gallery__link">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>
+  </li>`;
+  })
+  .join('');
+ulList.insertAdjacentHTML('beforeend', markup);
+ulList.addEventListener('click', basicLight);
 
-console.log(galleryItems);
+function basicLight(limiter) {
+  const instance = basicLightbox.create(`
+  <img src="${limiter.target.dataset.source}" width="800" height="600">
+`);
+  instance.show();
+
+  document.addEventListener('keydown', e => {
+    if (e.code === 'Escape') {
+      instance.close();
+    }
+  });
+}
